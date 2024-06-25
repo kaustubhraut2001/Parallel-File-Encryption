d4c03ca3f39f8b0f28d8b93c080a710af821140f
kaustubhraut2001 <kaustubhraut2001@gmail.com>
2024-06-25 22:32:45 +0530

    completed the implemetion in IO file

---
+++ src/app/filehandeling/IO.cpp
@@ -0,0 +1,20 @@
+// this file is the implemetaion if the IO.hpp file
+#include <iostream>
+#include "IO.hpp"
+#include <fstream>
+
+// implemetaion of the constructor that we have declier in the IO.hpp file
+
+IO::IO(const std::string &file_path)
+{
+	fileStream.open(file_path, std::ios::in | std::ios::out | std::ios::binary); // these are input output functional calls we are doing here
+	if (!fileStream.is_open())													 // if we are facing any issue in opening the file
+	{
+		std::cout << "Error opening file" << file_path << std::endl;
+	}
+}
+
+fstream IO::getfileStream()
+{
+	return std::move(fileStream);
+}
--- src/app/filehandeling/IO.hpp
+++ src/app/filehandeling/IO.hpp
@@ -8,13 +8,15 @@
 class IO
 {
 public:
-	IO(const std::string &filepath) // we are making the condtrsucter it will take the file path here which will not change so we are making it const
-	~IO();							// as every constructor has an destructor method we are defingin it
+	IO(const std::string &filepath); // we are making the condtrsucter it will take the file path here which will not change so we are making it const
+	~IO();							 // as every constructor has an destructor method we are defingin it
 
 	std::fstream getfileStream();
 
 private:
 	std::fstream fileStream;
-}
+};
 
 #endif // ending of this code block
+
+// this file only consistes of the defination we are giving the implementation in IO.cpp file
