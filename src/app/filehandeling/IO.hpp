9ac120b11f7f1389d5daffcf6aec03e7cba51fcf
kaustubhraut2001 <kaustubhraut2001@gmail.com>
2024-06-25 22:22:07 +0530

    IO file created

---
+++ src/app/filehandeling/IO.hpp
@@ -0,0 +1,20 @@
+#ifndef IO_HPP // this mean if not define then we are defining it in the below line
+#define IO_HPP
+
+// including the header files
+#include <fstream>
+#include <string>
+
+class IO
+{
+public:
+	IO(const std::string &filepath) // we are making the condtrsucter it will take the file path here which will not change so we are making it const
+	~IO();							// as every constructor has an destructor method we are defingin it
+
+	std::fstream getfileStream();
+
+private:
+	std::fstream fileStream;
+}
+
+#endif // ending of this code block
