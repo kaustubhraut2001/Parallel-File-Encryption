b0ade99b36a1e7a2fc7e7c3638e70d4ca3d14e0b
kaustubhraut2001 <kaustubhr2001@gmail.com>
2024-06-27 22:12:30 +0530

    encryption code hpp and cpp file implemetaion

---
+++ src/app/encriptdecript/Cryption.cpp
@@ -0,0 +1,24 @@
+#include "../encriptdecript/Cryption.hpp"
+#include "../Process/Task.hpp"
+#include "../filehandeling/ReadEnv.cpp"
+
+int executeEncryption(const std::string &taskData)
+{
+	Task task = Task::fromString(taskData);
+	ReadEnv env;
+
+	std::string envKey = env.getKey();
+	int key = std::stoi(envkey);
+
+	if (task.action == Action::ENCRYPT)
+	{
+		char ch;
+		while (task.f_stream.get(ch))
+		{
+			ch = (ch + key) % 256;
+			task.f_stream.seekp(-1, std::ios::cur);
+			task.f_stream.put(ch);
+		}
+		task.f_stream.close();
+	}
+}
---
+++ src/app/encriptdecript/Cryption.hpp
@@ -0,0 +1,9 @@
+#ifndef CRYPTION_HPP
+#define CRYPTION_HPP
+
+#include <iostream>
+#include <string>
+
+int executeEncryption(const std::string &taskData);
+
+#endif
